e04a92dac40cb30be2765f6158e00b98ec6eaa3f
pikkapika <naveenrawatGT@gmail.com>
2020-09-09 17:38:41 +0530

    locusWebApp_001

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\91855\desktop\check\css workout\django_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
